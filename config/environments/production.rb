f5335a13ec911f3cc80320ac2112633266f50ea0
Christiano Milfont <cmilfont@gmail.com>
2013-10-20 10:28:34 -0300

    COnfigurando a variavel de ambiente para o EL em prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Gladiatoria::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  ENV['SEARCHBOX_URL'] = ENV['SEARCHBOX_URL'].gsub(/http:\/\//, '')
+
 end
